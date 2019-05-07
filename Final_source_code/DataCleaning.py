d74be77de6440978879bdd76dade8c865ccd4ad3
Sivaraman Lakshmipathy <sivaramanl@users.noreply.github.com>
2019-05-06 20:12:37 -0500

    Added data cleaning source code
    Added data cleaning source code (from progress report)

---
+++ Final_source_code/DataCleaning.py
@@ -0,0 +1,48 @@
+meta=meta[~(meta['AGE'].isnull())] # removes all samples without age
+
+counts=pd.DataFrame(meta['SMTS'].value_counts())
+
+#There are many tissues with >200 samples with age recorded. Only tissues with 200 samples or more will be considered for predictive analysis.
+
+df=meta[meta['SMTS'].isin(counts[counts['SMTS']>200].index)]
+df=pd.crosstab(index=df['SMTS'],columns=df['AGE'])
+
+#Filter data by expression level with the following function
+def filter_by_expr(counts,min_count=None,min_sample=None,grp=None):
+    lib_size=np.sum(counts,axis=1)
+    MedianLibSize=np.median(lib_size)
+    norm_cutoff=min_count/MedianLibSize*1e6
+    print(norm_cutoff)
+    gene_counts=np.sum(counts)
+    
+#Filter by row count threshold
+
+tissue_meta=meta[(meta['SMTS']==TISSUE)]
+tissue_meta.iloc[0:3]
+
+cdat_train, cdat_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(cdat, tissue_meta['AGE'], test_size=.3, random_state=1234)
+cpm_train, cpm_test, y_train, y_test = \
+        sklearn.model_selection.train_test_split(cpm, tissue_meta['AGE'], test_size=.3, random_state=1234) # random state guarantees that the same split is made for a given tissue.
+sum(cpm_train.iloc[:,0]) # Confirms that the split is the same each time
+
+# Biased against samples with a smaller library size
+def simpleExpressionFilter(counts,min_count):
+    """accepts raw counts and a minimum sum count per gene across all samples
+    return a boolean array of all genes, which can be applied to any transformed counts.
+    True is associated with passing the test.
+    """
+    keep=np.sum(counts)>min_count
+    filtered_counts=counts.loc[:,(keep)] # similar to how the boolean array would be used on any count matrix
+    return(keep)
+keep_expr=simpleExpressionFilter(cdat_train,10)
+cpm_train_expression_filter=cpm_train.loc[:,(keep_expr)]
+cpm_test_expression_filter=cpm_test.loc[:,(keep_expr)]
+
+#Filter by low variance variance and plot mean distribution of genes after each step explained above.
+
+selector=sklearn.feature_selection.VarianceThreshold(threshold=.1)
+selector.fit(cpm_train_expression_filter)
+var_keep=selector.get_support(indices=True)
+train_final=cpm_train_expression_filter.iloc[:,var_keep]
+test_final=cpm_test_expression_filter.iloc[:,var_keep]
