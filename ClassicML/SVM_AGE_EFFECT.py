864f19524901b89fbbaa4d402108759322a3ae3e
fmanuc2 <47614168+fmanuc2@users.noreply.github.com>
2019-05-07 11:31:12 -0500

    Plots for final report

--- ClassicML/SVM_AGE_EFFECT.py
+++ ClassicML/SVM_AGE_EFFECT.py
@@ -9,7 +9,6 @@ import sklearn
 import sklearn.model_selection
 import sklearn.feature_selection
 from os import listdir
-#%matplotlib inline
 from sklearn.decomposition import PCA
 
 
@@ -110,77 +109,15 @@ SVM_ACCURACY_FILE.to_csv('SVM_ACCURACY_FILE.csv')
 
 
 
-####### KNN
 
-
-accuracy_knn=[]
-for TISSUE in Tissue_list:
-    cpm=pd.read_csv(tissue_dir/str(TISSUE+"_cpm.tsv"),sep="\t",index_col=0)
-    #lcpm=pd.read_csv(tissue_dir/str(TISSUE+"_lcpm.tsv"),sep="\t",index_col=0)
-    cdat=pd.read_csv(tissue_dir/str(TISSUE+"_c.tsv"),sep="\t",index_col=0)
-    tissue_meta=meta[(meta['SMTS']==TISSUE)]
-    cdat_train, cdat_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(cdat, tissue_meta['AGE'], test_size=.3, random_state=1234)
-    cpm_train, cpm_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(cpm, tissue_meta['AGE'], test_size=.3, random_state=1234) # random state guarantees that the same split is made for a given tissue.
-    sum(cpm_train.iloc[:,0]) # Confirms that the split is the same each time
-    # Biased against samples with a smaller library size
-    keep_expr=simpleExpressionFilter(cdat_train,10)
-    cpm_train_expression_filter=cpm_train.loc[:,(keep_expr)]
-    cpm_test_expression_filter=cpm_test.loc[:,(keep_expr)]
-    selector=sklearn.feature_selection.VarianceThreshold(threshold=.1)
-    selector.fit(cpm_train_expression_filter)
-    var_keep=selector.get_support(indices=True)
-    train_final=cpm_train_expression_filter.iloc[:,var_keep]
-    test_final=cpm_test_expression_filter.iloc[:,var_keep]
-    X=train_final.append(test_final)
-    y=y_train.append(y_test)
-    X=X.values
-    y=y.values
-    pca=PCA(n_components=len(y))
-    X=pca.fit_transform(X)
-    #X.shape
-    total_number=len(y)
-    train_number=round(0.8*total_number)
-    random_vals=random.sample(range(total_number+1), train_number)
-    X_train=X[[i for i in range(0,total_number) if i in random_vals]]
-    X_test=X[[i for i in range(0,total_number) if i not in random_vals]]
-    y_train=y[[i for i in range(0,total_number) if i in random_vals]]
-    y_test=y[[i for i in range(0,total_number) if i not in random_vals]]
-    #['linear', 'rbf', 'poly', 'sigmoid']
-    init=sklearn.neighbors.KNeighborsClassifier(n_neighbors=6)
-    classifier=init.fit(X_train, y_train)
-    ev=classifier.predict(X_test)
-    ac=0
-    for i in range (0,len(y_test)):
-        if y_test[i]==ev[i]:
-            a=0
-        else:
-            a=1
-        ac=ac+a
-    ac=1-ac/len(y_test)
-    accuracy_knn.append(ac)
-
-    
-### Plotting the accuracy of our model for different tissue
-
-fig. axs=plt.subplots(1,2)
-
-sns.barplot(Tissue_list, accuracy, axs[0][0])
+sns.barplot(Tissue_list, accuracy)
 plt.title('Accuracy of Different Tissues for SVM method')
 plt.xlabel('Tissue Type')
 plt.ylabel('Accuracy')
 plt.xticks(rotation=30, ha='right')
 plt.savefig('SVM_Accuracy.png')
 
-sns.barplot(Tissue_list, accuracy_knn, axs[0][1])
-plt.title('Accuracy of Different Tissues for KNN method')
-plt.xlabel('Tissue Type')
-plt.ylabel('Accuracy')
-plt.xticks(rotation=30, ha='right')
-plt.savefig('KNN_Accuracy.png')
-
-
+plt.show()
 
 
 ### Exporting accuracy data as csv file
