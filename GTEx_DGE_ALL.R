b071ff60bcf216b51fe7175d5f053774248f496a
Benjamin Imlay <bimlay2@uic.edu>
2019-05-06 20:33:16 -0500

    further cleaning of r script

--- GTEx_DGE_ALL.R
+++ GTEx_DGE_ALL.R
@@ -8,8 +8,7 @@ library(stringr)
 biomart_file_path=file.path("annotation","2018-04-12_biomart_ensemblGene_hgnc_uniprot.tab")
 uniprot_meta_path=file.path("annotation","uniprot_meta.tsv")
 nCore<-6
-root_dir<-"/home/imlay/storage_2/misc_repos/cs418-project-RNAge/"
-data_dir<-"/home/imlay/storage_2/misc_repos/cs418-project-RNAge/data"
+data_dir<-"data"
 
 ### common functions
 filterBiomart <- function(infile) {
@@ -66,49 +65,12 @@ print(paste0("Output: ",nrow(DEG_counts)))
 DEG_lcpm<-cpm(DEG_counts,log = TRUE)
 
 ## DEG
-DEG_meta$SEX<-factor(DEG_meta$SEX) # 1 is male 2 is female
-levels(DEG_meta$SEX)<-list(M=c("1"),FE=c("2"))
-grouping <- with(data = DEG_meta,factor(paste0(AGE,"_", SEX)))
-#design<-model.matrix(~0+AGE+SEX:AGE,data=DEG_meta)
+
 design<-model.matrix(~0+AGE,data=DEG_meta)
 colnames(design)<-str_remove(colnames(design),"AGE")
-#design<-model.matrix(~0+grouping)
-#colnames(design)<-str_remove(colnames(design),"grouping")
-#colnames(design)<-gsub("\\:","",colnames(design))
 contr.matrix <- makeContrasts(
-#  #A4vA5=A4-A5,
-#  #A3vA4=A3-A4,
-#  #A3vA5=A3-A5,
-#  #A2vA3=A2-A3,
-#  A2vA4=A2-A4,
-#  A2vA5=A2-A5,
-#  #A1vA2=A1-A2,
-#  #A1vA3= A1-A3,
-#  A1vA4= A1-A4,
   LAvHA= (A1+A2+A3)/3-(A4+A5+A6)/3,
   levels = colnames(design))
-#contr.matrix <- makeContrasts(
-  #A4vA5=A4-A5,
-  #A3vA4=A3-A4,
-  #A3vA5=A3-A5,
-  #A2vA3=A2-A3,
-  #A3vA4_FE=A3_FE-A4_FE,
-  #A3vA5_FE=A3_FE-A5_FE,
-  #A3vA4_M=A3_M-A4_M,
-  #A3vA5_M=A3_M-A5_M,
-#  A2vA4_FE=A2_FE-A4_FE, # 5
-  #A2vA5_FE=A2_FE-A5_FE,
-  #A2vA4_M=A2_M-A4_M,
-  #A2vA5_M=A2_M-A5_M,
-  #A1vA2=A1-A2,
-  #A1vA3= A1-A3,
-  #A1vA3_FE=A3_FE-A4_FE,
-  #A1vA3_M=A3_M-A5_M,
-  #A1vA4_FE= A1_FE-A4_FE,
-  #A1vA5_FE= A1_FE-A5_FE,
-  #A1vA4_M= A1_FE-A4_M,
-  #A1vA5_M= A1_FE-A5_M,
-#  levels = colnames(design))
 png(filename = file.path("DGE_plots","Init_SA.png"))
 v <- voom(DEG_counts, design, plot=TRUE)
 dev.off()
